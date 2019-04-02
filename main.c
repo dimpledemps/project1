7430cbb1190b826e90e85598402895ec5e2b87f6
Natalia Dempsey <c3316625@uon.edu.au>
2019-04-02 23:33:55 +0000

    Messing around with things for assignment

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 
 int main(){
-    printf("Insert some stuff");
-    printf("Even more stuff than before");
-    printf("Including extra so I can do a commit");
-    printf("Please work");
+    
+    //switch	
+char c=0;
+//basic scaffold 
+//Rotation cipher where a=0,b=1,c=2...z=25
+
+
+
+
+
+switch(c)
+
+case 1:
+	printf("Please select an option");
+	break;
+case 2:
+	printf("Encryption using a rotation cipher ");
+	break;
+case 3:
+	printf("Decryption using a rotation cipher");
+	break;
+case 4:
+	printf("Encryption using a substitution cipher");
+	break;
+case 5:
+	printf("Decryption using a substitution cipher");
+	break;
+default:printf("No option selected, please select an option");
+	break;
+
+    //printf("Insert some stuff");
+    //printf("Even more stuff than before");
+    //printf("Including extra so I can do a commit");
+    //printf("Please work");
   return 0;
  }
