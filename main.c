50a4cfab3f5f3dadcefdd7d62c189afefcc9f37f
Natalia Dempsey <c3316625@uon.edu.au>
2019-04-02 22:28:19 +0000

    Fixed error with printf statement

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(){
     printf("Insert some stuff");
     printf("Even more stuff than before");
-    printf("Including extra so I can do a committ")
+    printf("Including extra so I can do a commit");
   return 0;
  }
